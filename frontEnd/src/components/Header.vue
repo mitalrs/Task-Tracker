1dd2c4a0d32d197442b82c5ef5ea771c46aaed39
mitalrs <mitalsapkale1999@gmail.com>
2022-11-21 11:37:09 +0530

    basic checking, project working properly

---
+++ frontEnd/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h2>Task Tracker</h2>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'header',
+}
+</script>
+
+<style scoped>
+ header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+ }
+</style>
