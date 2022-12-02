9518e33e46919c34f89ebc8e186c10e0845f7261
mitalrs <mitalsapkale1999@gmail.com>
2022-12-02 22:44:39 +0530

    done the task added after refreshing the page

--- frontEnd/src/App.vue
+++ frontEnd/src/App.vue
@@ -22,8 +22,18 @@ export default {
     toggleAddTask(){
       this.showAddTask = !this.showAddTask
     },
-    addTask(task){
-      this.tasks = [...this.tasks, task]
+     async addTask(task){
+      const res = await fetch(`${import.meta.env.VITE_api}/tasks`, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task)
+      })
+
+      const data = await res.json()
+
+      this.tasks = [...this.tasks, data]
     },
     deleteTask(id){
       if(confirm('Are you sure to delete?')){
